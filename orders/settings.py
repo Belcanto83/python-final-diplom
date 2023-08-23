3fc423066936f738ab3bcd68f9a119442f936408
Alex <farego@yandex.ru>
2023-08-23 12:21:14 +0300

    settings.py is changed: run project with Docker

--- orders/settings.py
+++ orders/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Выбираем нужный файл настроек среды: с Docker или без
-load_dotenv('.env.without_docker')
+load_dotenv('.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
